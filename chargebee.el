d3be69e180696cae1b644887fa824746a0432679
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2023-12-19 09:08:49 -0300

    fix get--attr function usage

--- chargebee.el
+++ chargebee.el
@@ -15,9 +15,9 @@
   :type 'string
   :group 'chargebee)
 
+(require 'load-relative)
 (require 'widget)
-(require 'chargebee-minor-mode (expand-file-name "./chargebee-minor-mode.el"
-						 (file-name-directory load-file-name)))
+(require-relative "./chargebee-minor-mode.el")
 
 (defgroup chargebee-faces nil
   "Chargebee Faces."
@@ -68,28 +68,28 @@
 	       response)))
 	(deferred:nextc it
 	  (lambda (response)
-	    (let ((customer (get--attr (request-response-data response) '(customer))))
+	    (let ((customer (get-attr (request-response-data response) '(customer))))
 	      (with-current-buffer buffer
-		(insert (format "Customer:   %s\n" (get--attr customer '(id))))
-		(insert (format "First Name: %s\n" (get--attr customer '(first_name))))
-		(insert (format "Last Name:  %s\n" (get--attr customer '(last_name))))
-		(insert (format "Email:      %s\n" (get--attr customer '(email))))
+		(insert (format "Customer:   %s\n" (get-attr customer '(id))))
+		(insert (format "First Name: %s\n" (get-attr customer '(first_name))))
+		(insert (format "Last Name:  %s\n" (get-attr customer '(last_name))))
+		(insert (format "Email:      %s\n" (get-attr customer '(email))))
 		(insert ?\n)
 		(insert ?\n)
 		(chargebee--add-section-head "Billing Address")
-		(insert (format "First Name: \t%s\n" (get--attr customer '(billing_address first_name))))
-		(insert (format "Last Name:  \t%s\n" (get--attr customer '(billing_address last_name))))
-		(insert (format "Company:    \t%s\n" (get--attr customer '(billing_address company))))
-		(insert (format "Email:      \t%s\n" (get--attr customer '(billing_address email))))
-		(insert (format "Phone:      \t%s\n" (get--attr customer '(billing_address phone))))
-		(insert (format "Line 1:     \t%s\n" (get--attr customer '(billing_address line1))))
-		(insert (format "Line 2:     \t%s\n" (get--attr customer '(billing_address line2))))
-		(insert (format "Line 3:     \t%s\n" (get--attr customer '(billing_address line3))))
-		(insert (format "City:       \t%s\n" (get--attr customer '(billing_address city))))
-		(insert (format "Zip:        \t%s\n" (get--attr customer '(billing_address zip))))
-		(insert (format "Country:    \t%s\n" (get--attr customer '(billing_address country))))
-		(insert (format "State:      \t%s\n" (get--attr customer '(billing_address state))))
-		(insert (format "Status:     \t%s\n" (get--attr customer '(billing_address validation_status))))
+		(insert (format "First Name: \t%s\n" (get-attr customer '(billing_address first_name))))
+		(insert (format "Last Name:  \t%s\n" (get-attr customer '(billing_address last_name))))
+		(insert (format "Company:    \t%s\n" (get-attr customer '(billing_address company))))
+		(insert (format "Email:      \t%s\n" (get-attr customer '(billing_address email))))
+		(insert (format "Phone:      \t%s\n" (get-attr customer '(billing_address phone))))
+		(insert (format "Line 1:     \t%s\n" (get-attr customer '(billing_address line1))))
+		(insert (format "Line 2:     \t%s\n" (get-attr customer '(billing_address line2))))
+		(insert (format "Line 3:     \t%s\n" (get-attr customer '(billing_address line3))))
+		(insert (format "City:       \t%s\n" (get-attr customer '(billing_address city))))
+		(insert (format "Zip:        \t%s\n" (get-attr customer '(billing_address zip))))
+		(insert (format "Country:    \t%s\n" (get-attr customer '(billing_address country))))
+		(insert (format "State:      \t%s\n" (get-attr customer '(billing_address state))))
+		(insert (format "Status:     \t%s\n" (get-attr customer '(billing_address validation_status))))
 		(insert ?\n)
 		(insert ?\n)))))
 	(deferred:nextc it
@@ -126,7 +126,7 @@
 		      :type "GET"
 		      :params `(
 				("customer_id[is]" . ,customer-id)
-				("limit" . "5")
+				("limit" . "10")
 				("offset" . ,(or offset "")))
 		      :headers `(("Content-Type" . "application/json")
 				 ,(chargebee--authorization-header))
@@ -135,7 +135,7 @@
   (deferred:nextc it
     (lambda (response)
       (let* ((invoices-response (request-response-data response))
-	     (invoices (get--attr invoices-response '(list))))
+	     (invoices (get-attr invoices-response '(list))))
 	(with-current-buffer buffer
 	  (chargebee--add-section-head (format "Invoices (%d)" (seq-length invoices)))
 	  (seq-do #'chargebee--write-invoice invoices)
@@ -151,7 +151,7 @@
 					   (chargebee--write-invoices buffer customer-id previous-offset pages-history)))
 			       "PREVIOUS")
 		(insert "  ")))
-	  (let ((next-offset (get--attr invoices-response '(next_offset))))
+	  (let ((next-offset (get-attr invoices-response '(next_offset))))
 	    (if next-offset
 		(progn
 		  (widget-create 'link
@@ -166,15 +166,15 @@
 	  (insert ?\n)))))))
 
 (defun chargebee--write-invoice (invoice)
-  (let ((currency-code (get--attr invoice '(invoice currency_code))))
-    (insert (propertize (format "%-10s" (get--attr invoice '(invoice id))) 
+  (let ((currency-code (get-attr invoice '(invoice currency_code))))
+    (insert (propertize (format "%-10s" (get-attr invoice '(invoice id))) 
 			'font-lock-face 'chargebee-yellow))
 
     (insert (format "%-20s %s %s"
 		    ;; TODO - 3600 hardcoded to solveconsider daylight saving, check how it's the appropriate way to handle it
-		    (format-time-string "%Y-%m-%dT%T" (get--attr invoice '(invoice date)) 3600)
+		    (format-time-string "%Y-%m-%dT%T" (get-attr invoice '(invoice date)) 3600)
 		    currency-code 
-		    (chargebee--to-decimal (get--attr invoice '(invoice total)))))
+		    (chargebee--to-decimal (get-attr invoice '(invoice total)))))
 
     (let ((refund-amount (chargebee--get-refund-amount-for-invoice invoice)))
       (if refund-amount
@@ -215,17 +215,26 @@
   (insert (propertize (format "> %s\n" text) 'font-lock-face 'chargebee-section-head)))
 
 (defun chargebee--get-refund-amount-for-invoice (invoice)
-  (let ((issued-credit-notes (get--attr invoice '(invoice issued_credit_notes)))
+  (let ((issued-credit-notes (get-attr invoice '(invoice issued_credit_notes)))
 	(amount 0))
     (seq-do
      (lambda (credit-note)
-       (if (string= "refunded" (get--attr credit-note '(cn_status)))
-	   (setq amount (+ amount (get--attr credit-note '(cn_total))))))
+       (if (string= "refunded" (get-attr credit-note '(cn_status)))
+	   (setq amount (+ amount (get-attr credit-note '(cn_total))))))
      issued-credit-notes)
     (if (> amount 0)
     	(chargebee--to-decimal amount))))
 
 (defun chargebee--to-decimal (x)
   (/ x 100.0))
+
+(defun get-attr (list keys)
+  "Access the nested attributes specified by KEYS in LIST."
+  (if keys
+      (let ((current (car keys)))
+        (if (listp list)
+            (get-attr (cdr (assoc current list)) (cdr keys))
+          (signal 'wrong-type-argument (list 'listp list))))
+    list))
 
 (provide 'chargebee)
